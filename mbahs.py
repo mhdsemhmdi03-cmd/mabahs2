b68056d1ebbc562b115a77c88682e9794ca38f27
mhdse mhmdi <mhdsemhmdi03@gmail.com>
2026-02-23 21:33:12 +0330

    Add simple Python file

---
+++ mbahs.py
@@ -0,0 +1,8 @@
+# Simple Python project for personal site
+
+def main():
+    print("Welcome to my personal project!")
+    print("This project is created with Git and GitHub.")
+
+if __name__ == "__main__":
+    main()
